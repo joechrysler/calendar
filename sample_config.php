e219a503cf01df0c7f1b3fe2dccc158d46319b2d
Joseph Chysler <jdchrysl@svsu.edu>
2011-03-25 13:06:15 -0400

    add sample config

---
+++ sample_config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Database Host  --  mysql
+//================================================+
+  $db_host        = 'database server';
+  $db_name        = 'database name';
+  $db_user        = 'username';
+  $db_pass        = 'password shhhhh...';
+  $fieldsDBname   = 'name for fields database';
+
+?>
